20c948e5a83b1484aa0a900455deff80dc0a0691
pankaj7464 <pkumar44676@gmail.com>
2024-02-29 07:47:47 -1100

    Initial Commit

---
+++ Employee.cs
@@ -0,0 +1,43 @@
+﻿namespace EmployeeManagement
+{
+    public class Employee
+    {
+        private int Id;
+        private string Name;
+        private string DepartmentName;
+
+        public event EventHandler OnEvent;
+
+        public Employee(int id, string name, string departmentName)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.DepartmentName = departmentName;
+        }
+        public int GetId()
+        {
+            OnEvenFired("GetId() method called");
+            return Id;
+        }
+
+        public string GetName()
+        {
+            OnEvenFired("GetName() method called");
+            return Name;
+        }
+
+        public string GetDepartmentName()
+        {
+
+            OnEvenFired("GetDepartmentName() method called");
+            return DepartmentName;
+        }
+
+        private void OnEvenFired(string message)
+        {
+            OnEvent?.Invoke(this, new EventArgs());
+            Console.WriteLine("____________________________________");
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace EmployeeManagement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Employee details:");
+            Console.WriteLine("=======================================");
+
+
+
+            Console.Write("Enter Id: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Department Name: ");
+            string departmentName = Console.ReadLine();
+
+            Employee employee = new Employee(id,name,departmentName);
+
+
+            Console.WriteLine("====================================");
+            Console.WriteLine("Employee Id"+ employee.GetId());
+
+
+            Console.WriteLine("====================================");
+            Console.WriteLine("Employee Id"+ employee.GetName());
+
+            Console.WriteLine("====================================");
+            Console.WriteLine("Employee Id" + employee.GetDepartmentName());
+
+
+
+        }
+    }
+}
