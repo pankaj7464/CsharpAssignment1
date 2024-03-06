3fa3e23aedc708dd0c71cae5fc830c8cc6e082f9
Pankaj Kumar <pkumar44676@gmail.com>
2024-03-06 13:14:53 +0530

    Assingment done

--- obj/Release/net8.0/EmployeeManagement.AssemblyInfo.cs
+++ obj/Release/net8.0/EmployeeManagement.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20c948e5a83b1484aa0a900455deff80dc0a0691")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement")]
